e640b923b3944a301fbe2e3dadf2618dcbd827d1
Syouya Takahashi <e185750@ie.u-ryukyu.ac.jp>
2018-11-24 01:37:36 +0900

    1st commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main(String[] args) {
         String str = null;
-        System.out.println(str.length());
+        int len = str.length();
+        System.out.println(len);
     }
 }
