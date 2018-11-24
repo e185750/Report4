7170f1b857d61135d3fc6d3ce09bd67e6bad3108
Syouya Takahashi <e185750@ie.u-ryukyu.ac.jp>
2018-11-25 02:19:16 +0900

    1st commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        try {
-            int len = str.length();
-            System.out.println(len);
-        } catch (NullPointerException e) {
-            System.out.println("例外が発生しました。");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        Integer.parseInt("百二十三");
     }
-
-
 }
