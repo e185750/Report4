c3cf1edf6c22b8f7f7e4363804e59202d226f2fc
Syouya Takahashi <e185750@ie.u-ryukyu.ac.jp>
2018-11-25 01:44:53 +0900

    1st commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
         String str = null;
-        int len = str.length();
-        System.out.println(len);
+        try {
+            int len = str.length();
+            System.out.println(len);
+        } catch (NullPointerException e) {
+            System.out.println("例外が発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
+
+
 }
